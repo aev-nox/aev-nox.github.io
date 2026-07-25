6e2bdf1f2d4325572476deb69de1bcd2cf36195a
aev-nox <synapse.turbo@gmail.com>
2026-07-25 06:34:36 +0300

    Create crypto.js

---
+++ js/crypto.js
@@ -0,0 +1,13 @@
+async function sha256(message) {
+    const msgBuffer = new TextEncoder().encode(message);
+    const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
+    return Array.from(new Uint8Array(hashBuffer)).map(b => b.toString(16).padStart(2, '0')).join('');
+}
+
+const encodeBase64 = (str) => btoa(encodeURIComponent(str));
+const decodeBase64 = (b64) => decodeURIComponent(atob(b64));
+
+function formatTime(ts) {
+    if(!ts) return "Никогда";
+    return new Date(ts).toLocaleString('ru-RU', {day:'2-digit', month:'2-digit', hour:'2-digit', minute:'2-digit'});
+}
