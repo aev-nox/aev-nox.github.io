71e3345187faba57c70dcf43baa9d4d3dc9107c6
aev-nox <synapse.turbo@gmail.com>
2026-07-26 06:38:11 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,4 @@
+// Настройка базы и глобальные константы
 const firebaseConfig = {
     apiKey: "AIzaSyAzCfA19BfslrhUnFBYOG72Gnd5lm_5YtI",
     authDomain: "global-student-project.firebaseapp.com",
@@ -13,6 +14,7 @@ const DOMAINS = [
     "https://my-secret-domain.com/"
 ];
 
+// Дефолтная ссылка-мастер (можно сменить в админке)
 const DEFAULT_MASTER_TOKEN = "INIT-ADMIN-KEY-8f3a9b1c7d2e4f5a";
 
 async function ensureMasterKeyExists() {
@@ -24,6 +26,7 @@ async function ensureMasterKeyExists() {
 }
 ensureMasterKeyExists();
 
+// Глобальная функция проверки прав
 async function isRealAdmin(userHash) {
     if (!userHash) return false;
     const snap = await db.ref(`admins/${userHash}`).once('value');
